8c76088bfbb783915262545ef1303f89b054078e
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-07 22:27:51 +0100

    cp pil /usr/lib/python3.7/site-packages/

--- alienrecognizer/src/AlienRecognitionKerasApp.py
+++ alienrecognizer/src/AlienRecognitionKerasApp.py
@@ -5,12 +5,12 @@ from keras.applications.resnet50 import preprocess_input
 from keras.models import load_model, model_from_json
 
 # architecture and weights from HDF5
-model = load_model('models/keras/model.h5')
+model = load_model('./models/keras/model.h5')
 
 # architecture from JSON, weights from HDF5
-with open('models/keras/architecture.json') as f:
+with open('./models/keras/architecture.json') as f:
     model = model_from_json(f.read())
-model.load_weights('models/keras/weights.h5')
+model.load_weights('./models/keras/weights.h5')
 
 
 validation_img_paths = []
