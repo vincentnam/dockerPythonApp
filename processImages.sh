781cb271d8a87486c754bd48a9a35dc6f4d10d78
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-07 17:51:40 +0100

    test build + test app

---
+++ processImages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+until [ ! -f ./alienrecognizer/data/imagetoprocess/imagetoProcess ]
+do
+
+done
+
+python3 alienrecognizer/src/AlienRecognitionKerasApp $(cat ./alienrecognizer/data/imagetoprocess/imagetoProcess) > rep.txt
+
