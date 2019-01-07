5564afd51c0c6fd995c1b5416bb121cd963a6bc4
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-07 23:25:36 +0100

    niandqsdkl

--- alienrecognizer/src/AlienRecognitionKerasApp.py
+++ alienrecognizer/src/AlienRecognitionKerasApp.py
@@ -5,12 +5,12 @@ from keras.applications.resnet50 import preprocess_input
 from keras.models import load_model, model_from_json
 
 # architecture and weights from HDF5
-model = load_model('model.h5')
+#model = load_model('model.h5')
 
 # architecture from JSON, weights from HDF5
-with open('architecture.json') as f:
-    model = model_from_json(f.read())
-model.load_weights('weights.h5')
+#with open('architecture.json') as f:
+#    model = model_from_json(f.read())
+#model.load_weights('weights.h5')
 
 
 validation_img_paths = []
@@ -18,14 +18,14 @@ for i in range(len(sys.argv)-1):
     validation_img_paths.append("../data/imagetoprocess/" + sys.argv[i+1])
 
 img_list = [Image.open(img_path) for img_path in validation_img_paths]
+print( img_list)
+#validation_batch = np.stack([preprocess_input(np.array(img.resize((255, 255))))
+  #                           for img in img_list])
 
-validation_batch = np.stack([preprocess_input(np.array(img.resize((255, 255))))
-                             for img in img_list])
+#pred_probs = model.predict(validation_batch)
 
-pred_probs = model.predict(validation_batch)
-
-for i, img in enumerate(img_list):
-    print(" {:.0f}% Alien, {:.0f}% Predator".format(100*pred_probs[i,0],100*pred_probs[i,1]))
+#for i, img in enumerate(img_list):
+ #   print(" {:.0f}% Alien, {:.0f}% Predator".format(100*pred_probs[i,0],100*pred_probs[i,1]))
 
 
 
