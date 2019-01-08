24d0db98aa5906d1fa9ce62cba42559cf292c314
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-08 14:00:37 +0100

    echo

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM gw000/keras:1.2.1-py3-tf-cpu
 
 WORKDIR /app
 ADD . /app/
-COPY /tmp/IPManager ./
 RUN pip3 install pillow
 CMD [ "chmod" ,"+x", "/app/processImages.sh"]
 ENTRYPOINT ["./processImages.sh"] 
--- processImages.sh
+++ processImages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+echo $SWARM_HOST
 while true; do
     ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
     $IPMana= $(cat /tmp/IPManager)
     until scp fedora@$IPMana:/worker1/imagetoprocess /tmp/ ;
     do
-    	scp fedora@IPMana:/(uname -n | cut -d'.' -f1)/ /app/alienrecognizer/data/imagetoprocess
+    	#scp fedora@IPMana:/(uname -n | cut -d'.' -f1)/ /app/alienrecognizer/data/imagetoprocess
+        :
     done
     python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
 
