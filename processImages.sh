96448129ee473996acbbefc388d250d3e07feec3
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-08 14:53:07 +0100

    echo

--- processImages.sh
+++ processImages.sh
@@ -3,13 +3,13 @@ echo $SWARM_HOST
 sleep 10
 while true; do
     sleep 5
-    ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
+    #ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
     $IPMana= $(cat /tmp/IPManager)
-    until scp fedora@$IPMana:/worker1/imagetoprocess /tmp/ ;
-    do
-    	#scp fedora@IPMana:/(uname -n | cut -d'.' -f1)/ /app/alienrecognizer/data/imagetoprocess
-        :
-    done
-    python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
+  #  until scp fedora@$IPMana:/worker1/imagetoprocess /tmp/ ;
+   # do
+    #	#scp fedora@IPMana:/(uname -n | cut -d'.' -f1)/ /app/alienrecognizer/data/imagetoprocess
+     #   :
+    #done
+    #python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
 
 done
