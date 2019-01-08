669ac71bde51fc4f5ef62bc66fe10a1235a42710
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-08 15:23:21 +0100

    passage image par github

--- processImages.sh
+++ processImages.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-echo $SWARM_HOST
-sleep 10
 while true; do
-    sleep 5
-    echo $SWARM_HOST
-    #ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
-    $IPMana= $(cat /tmp/IPManager)
-  #  until scp fedora@$IPMana:/worker1/imagetoprocess /tmp/ ;
-   # do
-    #	#scp fedora@IPMana:/(uname -n | cut -d'.' -f1)/ /app/alienrecognizer/data/imagetoprocess
-     #   :
-    #done
-    #python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
+    ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
 
+
+    $IPMana= $(cat /tmp/IPManager)
+
+    git clone https://github.com/vincentnam/worker$(uname -n | cut -d"." -f1 | cut -d"-" -f4)
+    python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
+    sleep 10
 done
