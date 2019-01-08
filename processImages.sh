a9c5e04cb976708068ab841f5a5d36e14026448e
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-01-08 15:27:06 +0100

    passage image par github

--- processImages.sh
+++ processImages.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 while true; do
     ls && ls alienrecognizer/ && ls alienrecognizer/src && ls alienrecognizer/data/imagetoprocess
-
-
-    $IPMana= $(cat /tmp/IPManager)
-
     git clone https://github.com/vincentnam/worker$(uname -n | cut -d"." -f1 | cut -d"-" -f4)
     python3 alienrecognizer/src/AlienRecognitionKerasApp.py $(cat alienrecognizer/data/imagetoprocess/imagetoProcess)
     sleep 10
